8eadb516cbda19ffebeb4de334cf746ecedc889b
Joao Augusto <augustoj311@gmail.com>
2022-05-28 23:52:29 -0300

    feat: adding meta tags

--- src/pages/animal/[animal].tsx
+++ src/pages/animal/[animal].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPropsContext } from 'next'
+import Head from 'next/head'
 import { Detail } from 'src/styles/animals'
 import * as S from 'src/styles/animal'
 import { InterrogationIcon } from 'public/detailsIcons/interrogation'
@@ -39,82 +40,123 @@ const AnimalPage: React.FC<AnimalInterface> = ({ animal }) => {
   }, [])
 
   return (
-    <Template
-      paths={[
-        { path: '/animals', label: 'Adote um amigo' },
-        { path: `/animal/${animal?.id}`, label: animal?.name },
-      ]}
-      title={animal?.name}
-      showTitle={false}
-    >
-      <S.FlexWrapper>
-        <S.LeftContainer>
-          <S.Title>{animal?.name}</S.Title>
-          <S.SubTitle>Como ele é?</S.SubTitle>
-          <S.Description>{animal?.description}</S.Description>
-          <S.SubTitle>Quais suas características?</S.SubTitle>
-          <S.DetailsContainer>
-            <div className="left">
-              <Detail>
-                <InterrogationIcon />
-                <p>{animal?.species}</p>
-              </Detail>
-              <Detail>
-                <BreedIcon />
-                <p>{animal?.breed}</p>
-              </Detail>
-              <Detail>
-                <SizeIcon />
-                <p>{animal?.size}</p>
-              </Detail>
-              <Detail>
-                <GenderIcon />
-                <p>{animal?.sex}</p>
-              </Detail>
-            </div>
+    <>
+      <Head>
+        <meta
+          prefix="og: http://ogp.me/ns#"
+          property="og:title"
+          content={animal?.name}
+        />
+        <meta
+          name="image"
+          prefix="og: http://ogp.me/ns#"
+          property="og:image"
+          content={image}
+        />
+        <meta
+          prefix="og: http://ogp.me/ns#"
+          property="og:description"
+          content={animal?.description}
+        />
+        <meta
+          prefix="og: http://ogp.me/ns#"
+          property="og:image:width"
+          content="1484"
+        />
+        <meta
+          prefix="og: http://ogp.me/ns#"
+          property="og:image:height"
+          content="1200"
+        />
+        <meta
+          prefix="og: http://ogp.me/ns#"
+          name="twitter:card"
+          content="summary_large_image"
+        />
+      </Head>
+      <Template
+        paths={[
+          { path: '/animals', label: 'Adote um amigo' },
+          { path: `/animal/${animal?.id}`, label: animal?.name },
+        ]}
+        title={animal?.name}
+        showTitle={false}
+      >
+        <S.FlexWrapper>
+          <S.LeftContainer>
+            <S.Title>{animal?.name}</S.Title>
+            <S.SubTitle>Como ele é?</S.SubTitle>
+            <S.Description>{animal?.description}</S.Description>
+            <S.SubTitle>Quais suas características?</S.SubTitle>
+            <S.DetailsContainer>
+              <div className="left">
+                <Detail>
+                  <InterrogationIcon />
+                  <p>{animal?.species}</p>
+                </Detail>
+                <Detail>
+                  <BreedIcon />
+                  <p>{animal?.breed}</p>
+                </Detail>
+                <Detail>
+                  <SizeIcon />
+                  <p>{animal?.size}</p>
+                </Detail>
+                <Detail>
+                  <GenderIcon />
+                  <p>{animal?.sex}</p>
+                </Detail>
+              </div>
 
-            <div className="right">
-              <Detail>
-                <VaccinesIcon />
-                <p>Vacinado</p>
-              </Detail>
+              <div className="right">
+                <Detail>
+                  <VaccinesIcon />
+                  <p>Vacinado</p>
+                </Detail>
 
-              <Detail>
-                <PlusIcon />
-                <p>
-                  {animal?.is_castrated === 'Sim' ? 'Castrado' : 'Não castrado'}
-                </p>
-              </Detail>
+                <Detail>
+                  <PlusIcon />
+                  <p>
+                    {animal?.is_castrated === 'Sim'
+                      ? 'Castrado'
+                      : 'Não castrado'}
+                  </p>
+                </Detail>
 
-              <Detail>
-                <SearchIcon />
-                <p>{animal?.personality}</p>
-              </Detail>
-            </div>
-          </S.DetailsContainer>
-          {alreadyAdopted ? (
-            <>
-              <S.SubTitle>Pedido de adoção enviado</S.SubTitle>
-              <S.Description>
-                Seu pedido de adoção foi enviado, agora é só aguardar que daqui
-                a pouco a gente entra em contato com você!
-              </S.Description>
-            </>
-          ) : (
-            <Button
-              buttonText="Quero adotar ele(a)"
-              href={`/animal/adopt/${animal?.id}`}
+                <Detail>
+                  <SearchIcon />
+                  <p>{animal?.personality}</p>
+                </Detail>
+              </div>
+            </S.DetailsContainer>
+            {alreadyAdopted ? (
+              <>
+                <S.SubTitle>Pedido de adoção enviado</S.SubTitle>
+                <S.Description>
+                  Seu pedido de adoção foi enviado, agora é só aguardar que
+                  daqui a pouco a gente entra em contato com você!
+                </S.Description>
+              </>
+            ) : (
+              <Button
+                buttonText="Quero adotar ele(a)"
+                href={`/animal/adopt/${animal?.id}`}
+              />
+            )}
+          </S.LeftContainer>
+          <S.ImageContainer>
+            <S.Image
+              placeholder="blur"
+              onError={handleErrorImage}
+              src={image}
             />
-          )}
-        </S.LeftContainer>
-        <S.ImageContainer>
-          <S.Image placeholder="blur" onError={handleErrorImage} src={image} />
-          <S.Background>
-            <BackgroundImage />
-          </S.Background>
-        </S.ImageContainer>
-      </S.FlexWrapper>
-    </Template>
+            <S.Background>
+              <BackgroundImage />
+            </S.Background>
+          </S.ImageContainer>
+        </S.FlexWrapper>
+      </Template>
+    </>
   )
 }
 
