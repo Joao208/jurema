90325b7241929f34a791af6aee31138c5bda759e
Joao Augusto <augustoj311@gmail.com>
2022-06-11 10:13:09 -0300

    fix: small fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['projeto-jurema.s3.amazonaws.com'],
+    domains: [
+      'projeto-jurema.s3.amazonaws.com',
+      'projeto-jurema.s3.us-east-2.amazonaws.com',
+    ],
   },
 }
 
