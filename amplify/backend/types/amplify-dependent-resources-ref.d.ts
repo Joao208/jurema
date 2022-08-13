38b3c838bb6446e9c7b0f23e84cb0c41ee70fece
Joao Augusto <augustoj311@gmail.com>
2022-08-13 03:34:07 -0300

    fix: fixing sitemap and adding staging

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "projetojuremasite7edc297a": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "storage": {
-        "s3ab405379": {
-            "BucketName": "string",
-            "Region": "string"
-        }
-    },
-    "api": {
-        "ProjetoJuremaSite": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
     "function": {
         "projetojuremasitede451c9b": {
             "Name": "string",
--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -7,18 +7,18 @@ import { Animal } from './animals'
 function generateSiteMap(animals: Animal[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
      <url>
        <loc>https://projetojurema.org</loc>
      </url>
      ${animals
-       .map(({ id }) => {
-         return `
+       .map(
+         ({ id }) =>
+           `
        <url>
            <loc>${`https://projetojurema.org/animal/${id}`}</loc>
        </url>
      `
-       })
+       )
        .join('')}
    </urlset>
  `
